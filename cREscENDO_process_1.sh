b5e718e20c362cd1e87e250e4d52173528759e0b
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-07 17:47:38 +0900

    Fixed a bug

--- cREscENDO_process_1.sh
+++ cREscENDO_process_1.sh
@@ -1,11 +1,11 @@
 samplename=$1
 inputname=$2
 
-python 0_preparation.py ${samplename} ${inputname}
-python 1_peak_extended.py ${samplename}
-bash 2_fastafrombed_extend.sh ${samplename}
-python 3_fastatoread_extend.py ${samplename}
-python 4_make_genelist.py ${samplename}
-python 5_testtrain_split.py ${samplename}
-python 5_gex_preprocess.py ${samplename} ${inputname}
-python 6_PCA_rna_embedding_raw.py ${samplename}
+python script/0_preparation.py ${samplename} ${inputname}
+python script/1_peak_extended.py ${samplename}
+bash script/2_fastafrombed_extend.sh ${samplename}
+python script/3_fastatoread_extend.py ${samplename}
+python script/4_make_genelist.py ${samplename}
+python script/5_testtrain_split.py ${samplename}
+python script/5_gex_preprocess.py ${samplename} ${inputname}
+python script/6_PCA_rna_embedding_raw.py ${samplename}
