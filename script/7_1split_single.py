3ccb4171748a1abad09533d5d598ebbdf35c6ef5
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-08 01:20:09 +0900

    fix a bug

--- script/7_1split_single.py
+++ script/7_1split_single.py
@@ -6,13 +6,13 @@ samplename=str(args[1])
 chanknum=10
 
 i=0
-command = ["python","7_2DNAembed.py",samplename,str(i)]
+command = ["python","script/7_2DNAembed.py",samplename,str(i)]
 subprocess.call(command)
-command = ["python","7_3RNAprediction_t2_raw_2.py",samplename,str(i)]
+command = ["python","script/7_3RNAprediction_t2_raw_2.py",samplename,str(i)]
 subprocess.call(command)
-command = ["python","7_4RNAprediction_load_t2_raw_2.py",samplename,str(i)]
+command = ["python","script/7_4RNAprediction_load_t2_raw_2.py",samplename,str(i)]
 subprocess.call(command)
-command = ["python","7_5RNAprediction_load_predrna_t2_raw_2.py",samplename,str(i)]
+command = ["python","script/7_5RNAprediction_load_predrna_t2_raw_2.py",samplename,str(i)]
 subprocess.call(command)
-command = ["python","7_6DeepLift_full_split_single.py",samplename,str(1000),str(i)]
+command = ["python","script/7_6DeepLift_full_split_single.py",samplename,str(1000),str(i)]
 subprocess.call(command)
