a3f66faa550778cf5b57da889fc71ccd3f6646dd
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-12 06:36:18 +0900

    first commit

---
+++ cREscENDO_process_1_glioma.sh
@@ -0,0 +1,17 @@
+samplename=$1
+inputname=$2
+genomepath=$3
+prefix=$4
+dir=$(cd $(dirname $0); pwd)
+
+cd ${dir}
+python glioma/0_preparation.py ${samplename} ${inputname} ${prefix}
+bash glioma/0a_liftover.sh ${samplename}
+python glioma/0a_hg19tohg38.py ${samplename}
+python script/1_peak_extended.py ${samplename}
+bash script/2_fastafrombed_extend.sh ${samplename} ${genomepath}
+python script/3_fastatoread_extend.py ${samplename}
+python script/4_make_genelist.py ${samplename}
+python script/5_testtrain_split.py ${samplename}
+python script/5_gex_preprocess.py ${samplename} ${inputname}
+python script/6_PCA_rna_embedding_raw.py ${samplename}
