c8e9b708154a1b1b6f4f643de5d61a0aece8d2cc
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-11 18:40:30 +0900

    update

--- for_motif/13_enha_clus_select_deeplift_for_paper.py
+++ for_motif/13_enha_clus_select_deeplift_for_paper.py
@@ -7,60 +7,44 @@ args = sys.argv
 samplename=str(args[1])
 cellcluspath=str(args[2])
 
-peaknum=1000
 
-pairlist=pd.read_csv(samplename+"/pair_300000.csv",header=None,names=("gene","start","end","pos"))
-pairlist=pairlist[["start","end","pos"]].to_numpy()
-max_len=int((pairlist[:,1]-pairlist[:,0]).max()+1)
-
-RNAembed=pd.read_csv(samplename+"/pca_50dim_rnaembed_raw.txt",sep=" ",header=None)
-RNAembed=RNAembed.to_numpy()
-traincellnum=np.load(samplename+"/traincellnum.npy")
-
-fullcell=RNAembed.shape[0]
-traincell=traincellnum #10000 11830 11740
-testcell=fullcell #1893
-testtag=0
-
-df=pd.read_csv(samplename+"/Deeplift_enhamtx_t2_raw.csv", header=0 , index_col=0)
-df.columns.str.contains("promoter")
-df2=df.iloc[:,~df.columns.str.contains("promoter")]
-
-enha = sc.AnnData(df2, df2.index.to_frame(), df2.columns.to_frame())
-sc.tl.pca(enha, svd_solver="arpack")
-sc.pp.neighbors(enha, n_neighbors=10, n_pcs=40)
-sc.tl.umap(enha)
-
-sc.pl.umap(enha, frameon=False, save="pbmc_for_paper_enhancer.png")
-cellclus=pd.read_csv(cellcluspath,header=None)
+cellclus=pd.read_csv(cellcluspath,sep=",",header=None)
 cellclus=cellclus.to_numpy()
-cellclus=cellclus[:,0]-1
-cellclus=cellclus[testtag:testtag+testcell]
-enha.obs["rnacluster"] = pd.Categorical(list(cellclus))
+cellclus=cellclus[:,0]
 
-sc.pl.umap(enha, frameon=False, save=samplename+"/myplot_deeplift_out.png",color="rnacluster")
-sc.pl.umap(enha, frameon=False, save=samplename+"/myplot_deeplift.png",color="rnacluster",add_outline=True, legend_loc="on data")
-sc.tl.rank_genes_groups(enha, "rnacluster", method="wilcoxon")
 
-sc.pl.rank_genes_groups(enha, n_genes=25, sharey=False, save="DAE_deeplift_pbmc_for_paper.png")
+fname=samplename+"/Deeplift_full_ver2_all.npy"
+enhamtx=np.load(fname)
+grad=np.load(samplename+"/allgrad_ssep_max.npy")
 
-enha.uns["rank_genes_groups"]["names"]
-
-DAEmtx=pd.DataFrame(enha.uns["rank_genes_groups"]["names"])
-pvalall=pd.DataFrame(enha.uns["rank_genes_groups"]["pvals_adj"])
-fcall=pd.DataFrame(enha.uns["rank_genes_groups"]["logfoldchanges"])
-
-DAEmtx.to_csv(samplename+"/DAE_mtx_deeplift.csv",header=True, index=True)
-
-dae=pd.read_csv(samplename+"/DAE_mtx_deeplift.csv", header=0 , index_col=0)
-dae=dae[0:peaknum]
-dae_n=dae.to_numpy()
 fullgenename=pd.read_csv(samplename+"/pair_300000.csv",header=None,names=("gene","start","end","pos"))
 fullgenename=fullgenename["gene"]
 fullgenename=fullgenename.to_numpy()
 
+pairlist=pd.read_csv(samplename+"/pair_300000.csv",header=None,names=("gene","start","end","pos"))
+pairlist=pairlist[["start","end","pos"]].to_numpy()
 
-grad=np.load(samplename+"/allgrad_ssep_max.npy")
+pairlist_prom=pd.read_csv(samplename+"/pair_promoter.csv",header=None)
+pairlist_prom=pairlist_prom[[1,2,3]].to_numpy()
+
+ATAC_use=np.load(samplename+"/ATAC_pred_fold0.npy")
+ATAC_use=ATAC_use[:,:,0]
+
+ATAC_corr=np.zeros(grad.shape)
+for j in range(ATAC_corr.shape[0]):
+  print(j)
+  peakstart=int(pairlist[j,0])
+  peakend=int(pairlist[j,1])
+  peaknum_gene=peakend-peakstart+1
+  prompos=int(pairlist_prom[j,0])
+  enha=list(range(peakstart,(peakend+1)))
+  enha.remove(prompos)
+  ATAC_tmp=np.zeros((enhamtx.shape[1],enhamtx.shape[2]))
+  ATAC_tmp[:,0]=ATAC_use[prompos,:]
+  ATAC_tmp[:,1:peaknum_gene]=ATAC_use[enha,:].transpose(1,0)
+  for i in range(peaknum_gene):
+    ATAC_corr[j,i]=np.corrcoef([ATAC_tmp[:,i],enhamtx[j,:,i]])[0,1]
+
 
 thv=np.percentile(grad[grad!=0], [40])
 sthv=np.percentile(grad[grad!=0], [85])
@@ -71,8 +55,6 @@ promenhatag[(grad>sthv)&(ATAC_corr>0)]=4
 promenhatag[:,0]=3
 
 
-fname=samplename+"/Deeplift_full_ver2_all.npy"
-enhamtx=np.load(fname)
 
 enhathr=np.percentile(enhamtx.transpose(0,2,1),75,axis=2) #B,L
 enhathrmtx=(enhamtx.transpose(1,0,2)>enhathr).transpose(1,0,2) #B,C,L
@@ -83,6 +65,27 @@ enhausetagmtx=enhausemtx.transpose(1,0)>0.7
 
 np.save(samplename+'/enhausetagmtx.npy',enhausetagmtx)
 np.save(samplename+'/promenhatag.npy',promenhatag)
+
+
+
+enhamtx_n=enhamtx.copy()
+enhatmp=enhamtx.transpose(0,2,1)[(promenhatag==4)]
+enhatmp=(enhatmp-enhatmp.mean(axis=0))/enhatmp.std(axis=0)
+enhamtx_n.transpose(0,2,1)[(promenhatag==4)]=enhatmp
+
+enhamtxuse=enhamtx_n.transpose(0,2,1)/10
+enhamtxuse=enhamtxuse[(promenhatag==4)]
+enhamtxusepow=pow(2, enhamtxuse.transpose(1,0))
+enhaall = sc.AnnData(enhamtxusepow)
+sc.tl.pca(enhaall, svd_solver="arpack")
+sc.pp.neighbors(enhaall, n_neighbors=10, n_pcs=40)
+sc.tl.umap(enhaall)
+enhaall.obs["leiden"] = pd.Categorical(list(cellclus))
+sc.tl.rank_genes_groups(enhaall, "leiden", method="wilcoxon")
+
+DAEmtxall=pd.DataFrame(enhaall.uns["rank_genes_groups"]["names"])
+pvalall=pd.DataFrame(enhaall.uns["rank_genes_groups"]["pvals_adj"])
+fcall=pd.DataFrame(enhaall.uns["rank_genes_groups"]["logfoldchanges"])
 
 
 enhaclusgenemtxall_allenha=np.zeros((DAEmtxall.shape[1],promenhatag.shape[0],promenhatag.shape[1]))
@@ -98,3 +101,8 @@ for i in range(DAEmtxall.shape[1]):
 
 
 np.save(samplename+"/enha_clus_mtx_deeplift.npy",enhaclusgenemtxall_allenha)
+
+################
+
+
+
