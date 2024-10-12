adc95aba1015c49de6fccaad49fb620bafe6f6d2
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-12 17:51:36 +0900

    fix a bug

--- cREscENDO_process_1_glioma.sh
+++ cREscENDO_process_1_glioma.sh
@@ -13,5 +13,5 @@ bash script/2_fastafrombed_extend.sh ${samplename} ${genomepath}
 python script/3_fastatoread_extend.py ${samplename}
 python script/4_make_genelist.py ${samplename}
 python script/5_testtrain_split.py ${samplename}
-python glioma/5_gex_preprocess.py ${samplename} ${inputname}
+python glioma/5_gex_preprocess.py ${samplename} ${inputname} ${prefix}
 python script/6_PCA_rna_embedding_raw.py ${samplename}
