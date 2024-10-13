a446eb3ffa4ca7507dbe51a1248c0a516ec303e7
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-13 13:29:44 +0900

    fix a bug

--- pbmc/A2d_PCHIC_liftover.sh
+++ pbmc/A2d_PCHIC_liftover.sh
@@ -1,4 +1,4 @@
 samplename=$1
 
-liftOver ${samplename}/bait_hg19_tag.bed ${samplename}/hg19ToHg38.over.chain ${samplename}/bait_hg38_tag.bed bait_unlifted.bed
-liftOver ${samplename}/oe_hg19_tag.bed ${samplename}/hg19ToHg38.over.chain ${samplename}/oe_hg38_tag.bed oe_unlifted.bed
+liftOver ${samplename}/bait_hg19_tag.bed ${samplename}/hg19ToHg38.over.chain ${samplename}/bait_hg38_tag.bed ${samplename}/bait_unlifted.bed
+liftOver ${samplename}/oe_hg19_tag.bed ${samplename}/hg19ToHg38.over.chain ${samplename}/oe_hg38_tag.bed ${samplename}/oe_unlifted.bed
