a0fdd7aeeb7d407f6b38c6dac578e285d60afec6
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-13 15:10:29 +0900

    fix a bug

--- cREscENDO_process_celltypemotif_integratedgradient.sh
+++ cREscENDO_process_celltypemotif_integratedgradient.sh
@@ -1,13 +1,13 @@
+dir=$(cd $(dirname $0); pwd)
+
+cd ${dir}
+
 samplename=$1
 enhaclusfile=$2
 cellclusfile=$3
 celltype=$4
 prefix=$5
 motiffile=$6
-
-dir=$(cd $(dirname $0); pwd)
-
-cd ${dir}
 
 python for_motif/14_tfmodiscoinput_clip_new2.py ${samplename} ${enhaclusfile} ${cellcluspath} ${celltype} ${prefix}
 bash for_motif/modisco.sh ${samplename} ${prefix} ${motiffile}
