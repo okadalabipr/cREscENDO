9a6c50ee808f15b942c8cd83eaed548450e64aa5
ken-murakami <107227384+ken-murakami@users.noreply.github.com>
2024-10-20 21:24:39 +0900

    fix a bug

--- script/3_fastatoread_extend.py
+++ script/3_fastatoread_extend.py
@@ -25,13 +25,13 @@ for record in SeqIO.parse(samplename+"/peaks_extend.fasta", 'fasta'):
     seq = record.seq
 
     for j in range(len(seq)):
-        if(seq[j]=="A"):
+        if(seq[j]=="A")|(seq[j]=="a"):
             df[cp,j,0]=1
-        if(seq[j]=="C"):
+        if(seq[j]=="C")|(seq[j]=="c"):
             df[cp,j,1]=1
-        if(seq[j]=="G"):
+        if(seq[j]=="G")|(seq[j]=="g"):
             df[cp,j,2]=1
-        if(seq[j]=="T"):
+        if(seq[j]=="T")|(seq[j]=="t"):
             df[cp,j,3]=1
     cp=cp+1
 
